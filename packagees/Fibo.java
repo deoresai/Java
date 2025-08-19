6a4d83b632f7f79a0c55b41c8807eb89be22a9c9
sai deore <deoresai90@gmail.com>
2025-08-19 23:15:25 +0530

    Update Fibo.java

--- packagees/Fibo.java
+++ packagees/Fibo.java
@@ -5,11 +5,12 @@ public class Fibo{
         int a=0,b=1;
         System.out.println("Fibonacci searies is ");
         for (int i = 1; i <= n; i++) {
-            System.err.print(a+" ");
+            System.out.print(a+" ");
             int next=a+b;
             a=b;
             b=next;
 
         }
     }
+
 }
