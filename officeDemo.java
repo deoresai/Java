48f9f925d445389687026e31f6ef3e0cdd7da80f
sai deore <deoresai90@gmail.com>
2025-08-27 23:13:13 +0530

    Create officeDemo.java
     Define an abstract class Staff with protected members id and name. Define a 
    parameterized constructor. Define one subclass OfficeStaff with member 
    department. Create n objects of OfficeStaff and display all details.

---
+++ officeDemo.java
@@ -0,0 +1,53 @@
+/* Define an abstract class Staff with protected members id and name. Define a 
+parameterized constructor. Define one subclass OfficeStaff with member 
+department. Create n objects of OfficeStaff and display all details. */
+
+import java.util.Scanner;
+
+class Staff {
+    protected int id;
+    protected String name;
+
+    Staff(int id,String name) {
+        this.id=id;
+        this.name=name;
+    }
+    void display(){
+        System.out.println("id :  "+id);
+        System.out.println("name : "+name);
+    }
+    
+}
+class OfficeStaff extends Staff{
+    String Dept;
+    OfficeStaff(int id,String name,String Dept) {
+        super(id,name);
+        this.Dept=Dept;
+    }
+    void display(){        
+        System.out.println("Department : "+Dept);
+        super.display();
+    }
+    
+}
+class officeDemo{
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n,i,id;
+        String name,Dept;
+        System.out.println("Count of office staff ?");
+        n=sc.nextInt();
+    
+        OfficeStaff s[]=new OfficeStaff[n];
+        for ( i = 0; i < n; i++) {
+            System.out.println("Enter a id,name,department");
+            id=sc.nextInt();
+            name=sc.next();
+            Dept=sc.next();
+            s[i]=new OfficeStaff(id,name,Dept); 
+        }
+        for ( i = 0; i < n; i++) {
+            s[i].display();
+        }
+    }
+}
